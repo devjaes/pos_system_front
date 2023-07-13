10d6beb379f02bac2d0d14b45822203fcbfed754
SrPabvliss <pablomartinvillacres@gmail.com>
2023-07-12 23:17:32 -0500

    Login remake pending

--- src/app/ELogin/page.tsx
+++ src/app/ELogin/page.tsx
@@ -138,7 +138,7 @@ function Login() {
                         <div className="flex items-center justify-between mt-4">
                             <a href="#" className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500">Forget Password?</a>
 
-                            <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                            <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50" onClick={handleLogin}>
                                 Sign In
                             </button>
                         </div>
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,6 +5,66 @@ import { IOption } from "@/components/Nav";
 import { ClientsCompaniesProvider } from "@/context/ClientsCompaniesProvider";
 import { useRouter } from "next/navigation";
 import { ProvinceProvider } from "@/context/ProvinceProvider";
+import { PanelMenu } from 'primereact/panelmenu';
+import { MenuItem } from 'primereact/menuitem';
+
+const items: MenuItem[] = [
+  {
+    label: 'Productos',
+    icon: 'pi pi-fw pi-tag',
+    items: [
+      {
+        label: 'Agregar',
+        icon: 'pi pi-fw pi-plus',
+
+      },
+      {
+        label: 'Administrar',
+        icon: 'pi pi-fw pi-pencil'
+      },
+
+    ]
+  },
+
+  {
+    label: 'Usuarios',
+    icon: 'pi pi-fw pi-user',
+    items: [
+      {
+        label: 'Agregar',
+        icon: 'pi pi-fw pi-user-plus'
+      },
+      {
+        label: 'Administrar',
+        icon: 'pi pi-fw pi-pencil'
+      },
+
+    ]
+  },
+  {
+    label: 'Mi Empresa',
+    icon: 'pi pi-fw pi-shopping-bag',
+    items: [
+
+      {
+        label: 'Agregar Cliente',
+        icon: 'pi pi-fw pi-plus-circle'
+      },
+      {
+        label: 'Agregar Caja',
+        icon: 'pi pi-fw pi-plus-circle'
+      },
+      {
+        label: 'Agregar Sucursal',
+        icon: 'pi pi-fw pi-plus-circle'
+      },
+    ],
+  },
+  {
+    label: 'Configuración',
+    icon: 'pi pi-fw pi-wrench',
+  }
+];
 
 
 export default function AdminLayout({
@@ -20,18 +80,20 @@ export default function AdminLayout({
     router.push(`/`);
   };
 
-  const options: IOption[] = [
-    { label: "Cerrar sesión", redirect: handleLogout, type: "button" },
-  ];
   return (
     <>
       <div>
-        <Navegador options={options} imageRedirect="/admin" />
-        <ProvinceProvider>
-          <ClientsCompaniesProvider>
-            <main>{children}</main>
-          </ClientsCompaniesProvider>
-        </ProvinceProvider>
+        <div className='flex'>
+          <div className='min-h-screen '>
+            <div className='h-full'>
+              <PanelMenu model={items} className="w-52 md:w-25rem mb-4" />
+            </div>
+
+          </div>
+          <div>
+            {children}
+          </div>
+        </div>
       </div>
     </>
   );
