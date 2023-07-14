98ab56d25730895c52106ee324c0c9c834424de4
jaesbyp <merajair17@gmail.com>
2023-07-13 19:06:05 -0500

    Change tables to objects

--- src/store/types/Tables.ts
+++ src/store/types/Tables.ts
@@ -1,47 +1,59 @@
 export const TAXES = ["IVA", "ICE", "IRBPNR"];
 
 export const IVAS = [
-  "0%",
-  "12%",
-  "14%",
-  "No Objeto de Impuesto",
-  "Exento de IVA",
-  "IVA diferenciado",
-  "IVA variable",
+  { name: "0%", code: "0%" },
+  { name: "12%", code: "12%" },
+  { name: "14%", code: "14%" },
+  { name: "No Objeto de Impuesto", code: "No Objeto de Impuesto" },
+  { name: "Exento de IVA", code: "Exento de IVA" },
+  { name: "IVA diferenciado", code: "IVA diferenciado" },
+  { name: "IVA variable", code: "IVA variable" },
 ];
 
-export const ICE = ["0%", "5%", "10%", "15%", "20%"];
+export const ICE = [{ name: "0%", code: "0%" }];
 
-export const IRBPNR = ["0%", "1%", "2%", "3%", "4%", "5%"];
+export const IRBPNR = [{ name: "0%", code: "0%" }];
 
 export const IDENTIFICATION_TYPES = [
-  "RUC",
-  "CÉDULA",
-  "PASAPORTE",
-  "VENTA A CONSUMIDOR FINAL",
-  "IDENTIFICACIÓN DEL EXTERIOR",
+  { name: "RUC", code: "RUC" },
+  { name: "CÉDULA", code: "CÉDULA" },
+  { name: "PASAPORTE", code: "PASAPORTE" },
+  { name: "VENTA A CONSUMIDOR FINAL", code: "VENTA A CONSUMIDOR FINAL" },
+  { name: "IDENTIFICACIÓN DEL EXTERIOR", code: "IDENTIFICACIÓN DEL EXTERIOR" },
 ];
 
 export const RECEIPTS = [
-  "FACTURA",
-  "LIQUIDACIÓN DE COMPRA DE BIENES Y PRESTACIÓN DE SERVICIOS",
-  "NOTA DE CRÉDITO",
-  "NOTA DE DÉBITO",
-  "GUÍA DE REMISIÓN",
-  "COMPROBANTE DE RETENCIÓN",
+  { name: "FACTURA", code: "FACTURA" },
+  {
+    name: "LIQUIDACIÓN DE COMPRA DE BIENES Y PRESTACIÓN DE SERVICIOS",
+    code: "LIQUIDACIÓN DE COMPRA DE BIENES Y PRESTACIÓN DE SERVICIOS",
+  },
+  { name: "NOTA DE CRÉDITO", code: "NOTA DE CRÉDITO" },
+  { name: "NOTA DE DÉBITO", code: "NOTA DE DÉBITO" },
+  { name: "GUÍA DE REMISIÓN", code: "GUÍA DE REMISIÓN" },
+  { name: "COMPROBANTE DE RETENCIÓN", code: "COMPROBANTE DE RETENCIÓN" },
 ];
 
-export const EMISSIONS = ["Emisión normal"];
+export const EMISSIONS = [{ name: "Emisión normal", code: "Emisión normal" }];
 
-export const ENVIRONMENTS = ["Pruebas", "Producción"];
+export const ENVIRONMENTS = [
+  { name: "Pruebas", code: "Pruebas" },
+  { name: "Producción", code: "Producción" },
+];
 
 export const PAYMENTS = [
-  "SIN UTILIZACION DEL SISTEMA FINANCIERO",
-  "COMPENSACIÓN DE DEUDAS",
-  "TARJETA DE DÉBITO",
-  "DINERO ELECTRÓNICO",
-  "TARJETA PREPAGO",
-  "TARJETA DE CRÉDITO",
-  "OTROS CON UTILIZACIÓN DEL SISTEMA FINANCIERO",
-  "ENDOSO DE TÍTULOS",
+  {
+    name: "SIN UTILIZACION DEL SISTEMA FINANCIERO",
+    code: "SIN UTILIZACION DEL SISTEMA FINANCIERO",
+  },
+  { name: "COMPENSACIÓN DE DEUDAS", code: "COMPENSACIÓN DE DEUDAS" },
+  { name: "TARJETA DE DÉBITO", code: "TARJETA DE DÉBITO" },
+  { name: "DINERO ELECTRÓNICO", code: "DINERO ELECTRÓNICO" },
+  { name: "TARJETA PREPAGO", code: "TARJETA PREPAGO" },
+  { name: "TARJETA DE CRÉDITO", code: "TARJETA DE CRÉDITO" },
+  {
+    name: "OTROS CON UTILIZACIÓN DEL SISTEMA FINANCIERO",
+    code: "OTROS CON UTILIZACIÓN DEL SISTEMA FINANCIERO",
+  },
+  { name: "ENDOSO DE TÍTULOS", code: "ENDOSO DE TÍTULOS" },
 ];
