d17b4c6b46d360862890296d960991a74a257998
jaesbyp <merajair17@gmail.com>
2023-07-19 00:21:40 -0500

    Fix invoice view

--- src/app/user/invoice/page.tsx
+++ src/app/user/invoice/page.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import { useState, useEffect, useRef, MouseEvent } from "react";
+import { useState, useEffect, useRef } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import {
-  handleCreateProduct,
-  handleDeleteProduct,
-  handleGetAllProducts,
-} from "@/store/api/productApi";
-import { IProductCreate, IProductResponse } from "@/store/types/IProducts";
-import { InputText } from "primereact/inputtext";
+import { handleGetAllProducts } from "@/store/api/productApi";
+import { IProductResponse } from "@/store/types/IProducts";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import { ConfirmPopup, confirmPopup } from "primereact/confirmpopup";
 import { Toast } from "primereact/toast";
-import { IInputsForm } from "@/store/types/IForms";
-import { KeyFilterType } from "primereact/keyfilter";
-import { ICE, IRBPNR, IVAS, PAYMENTS } from "@/store/types/Tables";
-import ModifyDialog from "@/components/modifyDialog";
+import { IVAS } from "@/store/types/Tables";
 import { Controller, useForm } from "react-hook-form";
 import { Avatar } from "primereact/avatar";
 import { Dropdown } from "primereact/dropdown";
@@ -30,7 +22,6 @@ import { IStoreLocal } from "@/store/types/IStore";
 import { round } from "@/store/utils/uuid";
 
 const invoicer = () => {
-  const [product, setProduct] = useState<IProductResponse>();
   const [products, setProducts] = useState<IProductResponse[]>([]);
   const [productTable, setProductTable] = useState<IProductResponse[]>([]);
   const [customers, setCustomers] = useState<ICustomerResponse[]>([]);
@@ -48,19 +39,12 @@ const invoicer = () => {
   const [effect, setEffect] = useState<boolean>(false);
   const router = useRouter();
 
+  const toast = useRef<Toast>(null);
+
   const myIVAS = IVAS;
 
-  const {
-    register,
-    reset,
-    setValue,
-    handleSubmit,
-    getValues,
-    getFieldState,
-    watch,
-    control,
-    formState,
-  } = useForm();
+  const { reset, setValue, handleSubmit, watch, control, formState } =
+    useForm();
 
   useEffect(() => {
     const storeLocal = window.localStorage.getItem("storeLocal");
@@ -104,6 +88,18 @@ const invoicer = () => {
   }, []);
 
   useEffect(() => {
+    if (
+      watch("receivedCash") != undefined &&
+      watch("receivedCash") != null &&
+      total != 0
+    ) {
+      setChange(Number(watch("receivedCash")) - total);
+    } else {
+      setChange(0);
+    }
+  }, [watch("receivedCash"), total]);
+
+  useEffect(() => {
     let subtotal = 0;
     productTable.forEach((product) => {
       subtotal += round(
@@ -113,16 +109,24 @@ const invoicer = () => {
     setSubtotal(round(subtotal));
     setSubTotals(getSubTotals(productTable));
     setIvasObject(getIvasObject(productTable));
-
     const taxesTotal = round(
       getIvasObject(productTable)
-        .map((ivas: any) => Object.values(ivas))
+        .map((iva) => Object.values(iva)[0])
         .reduce((a: any, b: any) => a + b, 0)
     );
 
     setTotal(round(Number(subtotal) + Number(taxesTotal)));
-    console.log(getValues());
   }, [productTable, effect]);
+
+  const formatIvaPrint = (key: string) => {
+    if (key.split(" ").length == 1 && !key.split(" ")[0].includes("%")) {
+      return "IVA " + key + "%";
+    } else if (key.split(" ").length == 1 && key.split(" ")[0].includes("%")) {
+      return "IVA " + key;
+    } else {
+      return key;
+    }
+  };
 
   const getSubTotals = (productTable: IProductResponse[]) => {
     const subTotals = productTable.map((product) => {
@@ -138,17 +142,21 @@ const invoicer = () => {
 
   const getIvasObject = (productTable: IProductResponse[]) => {
     const ivaObject = myIVAS.map((iva: string) => {
-      return {
-        [iva]: getSubTotals(
-          productTable.filter(
-            (product) => !product.ivaVariable && product.ivaType == iva
-          )
-        )
-          .map((subTotal) => {
-            console.log(subTotal);
-            return round(Number(subTotal[Object.keys(subTotal)[0] as any]));
+      const t = getSubTotals(
+        productTable
+          .map((product) => {
+            if (!product.ivaVariable && product.ivaType == iva) {
+              return product;
+            }
           })
-          .reduce((a, b) => a + b, 0),
+          .filter((product) => product != undefined) as IProductResponse[]
+      )
+        .map((subTotal) => {
+          return round(Number(subTotal[Object.keys(subTotal)[0] as any]));
+        })
+        .reduce((a, b) => a + b, 0);
+      return {
+        [iva]: t,
       };
     });
 
@@ -200,7 +208,7 @@ const invoicer = () => {
           iva[key] = Number(0);
           break;
         case "IVA diferenciado":
-          iva[key] = round(value * 0.12);
+          iva[key] = 0;
           break;
         default:
           break;
@@ -258,16 +266,47 @@ const invoicer = () => {
     setProducts([...products, product]);
   };
 
-  const onSubmit = handleSubmit((data) => {
-    console.log({ data });
-    console.log(watch("quantity") as number);
-    console.log(
-      (products[0].unitPrice as number) * (watch("quantity") as number)
-    );
-  });
+  const confirm = (event: React.MouseEvent<HTMLButtonElement>) => {
+    confirmPopup({
+      target: event.currentTarget,
+      message: "¿Está seguro que quiere cancelar esta factura?",
+      icon: "pi pi-exclamation-triangle",
+      acceptClassName: "p-button-danger",
+      accept,
+      reject,
+    });
+  };
+
+  const accept = () => {
+    toast.current?.show({
+      severity: "info",
+      summary: "SConfirmed",
+      detail: "Ha cancelado la factura",
+      life: 3000,
+    });
+    setCustomer(undefined);
+    setValue("receivedCash", 0);
+    reset();
+    setChange(0);
+    setProductTable([]);
+    setDisables(false);
+    window.location.reload();
+  };
+
+  const reject = () => {
+    toast.current?.show({
+      severity: "info",
+      summary: "Rejected",
+      detail: "No se canceló la factura",
+      life: 3000,
+    });
+  };
+
+  const onSubmit = handleSubmit((data) => {});
 
   return (
     <div className="flex flex-col w-11/12 ">
+      <Toast ref={toast} />
       <h1 className="w-full border border-opacity-5 bg-gray-800 text-lg">
         <Avatar icon="pi pi-plus" className="bg-gray-700 m-2" />
         <strong>Nuevo registro de una factura</strong>
@@ -328,6 +367,11 @@ const invoicer = () => {
               maxFractionDigits={2}
               min={0.0}
               max={500.0}
+              onKeyDownCapture={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                }
+              }}
               onValueChange={(e) => {
                 setValue("receivedCash", e.value);
               }}
@@ -339,7 +383,17 @@ const invoicer = () => {
             </label>
             <div className="flex bg-jair-black  w-full h-full rounded-sm justify-center items-center">
               <label className="justify-center">
-                <strong>{change}</strong>
+                <strong>{change.toFixed(2)}</strong>
+              </label>
+            </div>
+          </div>
+          <div className="flex flex-col w-2/12 pr-6">
+            <label htmlFor="cash">
+              <strong>CAJA</strong>
+            </label>
+            <div className="flex bg-jair-black  w-full h-full rounded-sm justify-center items-center">
+              <label className="justify-center">
+                <strong>{storeLocal ? storeLocal.box.key : ""}</strong>
               </label>
             </div>
           </div>
@@ -489,22 +543,24 @@ const invoicer = () => {
                   {ivasObject.map((iva: any, index: number) => {
                     const key = Object.keys(iva)[0]; // Acceder a la única key del objeto
                     const value = iva[key];
-                    console.log({ ivasObject }, { total });
                     return (
                       <div
                         className="flex flex-row justify-between"
                         key={index}
                       >
-                        <p className="text-lg font-bold">{key}:</p>
+                        <p className="text-lg font-bold">
+                          {formatIvaPrint(key)}
+                        </p>
                         <label className="text-lg font-bold pl-4">
                           {value.toFixed(2)}
                         </label>
                       </div>
                     );
                   })}
+                  <br></br>
                   <div className="flex flex-row justify-between">
-                    <p className="text-lg font-bold">TOTAL: </p>
-                    <label className="text-lg font-bold pl-4">
+                    <p className="text-2xl font-bold">TOTAL: </p>
+                    <label className="text-2xl font-bold pl-4">
                       {total.toFixed(2)}
                     </label>
                   </div>
@@ -512,17 +568,12 @@ const invoicer = () => {
               </div>
             </div>
             <div className="flex flex-row justify-between gap-4">
+              <ConfirmPopup />
               <Button
                 className="p-button-danger p-mt-4  ml-4 w-full px-4"
                 label="Cancelar"
                 type="button"
-                onClick={() => {
-                  setCustomer(undefined);
-                  reset();
-                  setProductTable([]);
-                  setDisables(false);
-                  router.refresh();
-                }}
+                onClick={confirm}
               />
               <Button
                 className="p-button-success p-mt-4 w-full px-4"
