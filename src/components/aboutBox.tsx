2a529dce00a059793a9cd803ddf337291da31d0f
emilia <emi1213galarza@gmail.com>
2023-07-19 22:32:30 -0500

    Update about

--- src/components/aboutBox.tsx
+++ src/components/aboutBox.tsx
@@ -1,11 +1,12 @@
-function bloquesNosotros({ titulo, svg, texto }: { titulo: string, svg: any, texto: string }) {
+import 'primeicons/primeicons.css';
+function bloquesNosotros({ titulo, icon, texto }: { titulo: string, icon: any, texto: string }) {
     return (
-        <div className='flex flex-col justify-center items-center flex-1'>
-            <div className="flex-1">
-                {svg}
+        <div className='border-2 border-slate-800 p-6 rounded-md border-solid flex flex-col justify-center items-center flex-1'>
+            <div className="flex-1 p-1">
+             <i className={icon} style={{fontSize: "2rem"}}></i>
             </div>
             <div className="flex-1">
-                <h3 className="font-bold text-xl text-center">
+                <h3 className="font-bold text-xl text-center ">
                     {titulo}
                 </h3>
                 <p className='text-center my-4'>
